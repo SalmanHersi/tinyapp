68eb1bd4034bd462f7597b24bc791ee3eea77470
SalmanHersi <hersi.salman@gmail.com>
2023-08-09 17:21:25 -0400

    added URL short route

--- express-server.js
+++ express-server.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+  res.json(urlDatabase); //Key value pairs read by json
 });
 
 app.get("/hello", (req, res) => {
@@ -43,12 +43,39 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
-function generateRandomString() {
-  const str = Math.random().toString().substring(2, 8);
+app.get("/u/:id", (req, res) => {
+  const shortURL = req.params.id;
+  const longURL = urlDatabase[shortURL];
+
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.status(404).send("URL not found");
+  }
+});
+
+app.post("/urls", (req, res) => {
+  const templateVars = {
+    longURL: urlDatabase[req.params.id],
+    id: req.params.id,
+  };
+  console.log(req.body);
+  res.send("Ok");
+});
+
+const randomString = () => {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let str = "";
+
+  for (let i = 0; i < 6; i++) {
+    const index = Math.floor(Math.random() * chars.length);
+    str = str + chars.charAt(index);
+  }
+
   return str;
-}
+};
