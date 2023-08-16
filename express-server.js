b3e94c65f513b3aef76f5503ec91d839155c7a03
SalmanHersi <hersi.salman@gmail.com>
2023-08-16 16:11:33 -0400

    fixed naming for app.listen

--- express-server.js
+++ express-server.js
@@ -191,5 +191,5 @@ app.post("/logout", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Tiny app listening on port ${PORT}!`);
 });
